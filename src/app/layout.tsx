418e58a368f96ebd26b4eddd68855d35bd6451d2
robshow <robsonaraujocarmo@hotmail.com>
2025-07-11 11:50:22 -0400

    config: material ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import theme from "@/theme";
+import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${inter.className} antialiased`}>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+          </ThemeProvider>
+        </StyledEngineProvider>
       </body>
     </html>
   );
