cc50b1aff07b73ad8d99b5bef79bb917d024a87a
robshow <robsonaraujocarmo@hotmail.com>
2025-07-12 12:39:28 -0400

    feat: add image domains

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["www.houseofblanks.com"],
+    domains: ["www.houseofblanks.com", "slimages.macysassets.com"],
   },
 };
 
